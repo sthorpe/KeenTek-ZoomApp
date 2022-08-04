1c90915991012cfdf19b7aff64433f038ba939b5
Scott Thorpe <sthorpe@gmail.com>
2022-08-04 11:22:00 -0600

    Now listing Zoom Rooms and their devices

--- index.js
+++ index.js
@@ -1,37 +1,30 @@
 require('dotenv').config();
 const request = require('request');
 const jwt = require('jsonwebtoken');
+const config = require('./config');
 
-const creds = process.env.ZOOMAPIKEY+':'+process.env.ZOOMAPISECRET;
+//Use the ApiKey and APISecret from config.js
+const payload = {
+    iss: config.APIKey,
+    exp: ((new Date()).getTime() + 5000)
+};
+const token = jwt.sign(payload, config.APISecret);
 
-
-function getAccessToken(){
-  var options = {
-    method: 'POST',
-    // Use the `me` keyword for the request below.
-    url: `https://zoom.us/oauth/token?grant_type=account_credentials&account_id={${process.env.ACCOUNTID}}`,
-    headers: {
-      authorization: `Basic Base64Encoder(Pk2Yk1qMTWGjl48O3fwhZg:stAo8JEqxrU8SQqOj5QgH35w7oV5CcwI)`,
-    },
-  };
-
-  request(options, function (error, response, body) {
-    if (error) throw new Error(error);
-
-    console.log(body);
-  });
-}
-
-getAccessToken();
-
-function userInformation(){
+function getZoomRooms(){
   var options = {
     method: 'GET',
-    // Use the `me` keyword for the request below.
-    url: 'https://api.zoom.us/v2/users/me',
-    headers: {
-      authorization: `Bearer {${ accessToken }}`, // Do not publish or share your token with anyone.
+    url: `https://api.zoom.us/v2/rooms`,
+    qs: {
+      status: 'active'
     },
+    auth: {
+      'bearer': token
+    },
+    headers: {
+      'User-Agent': 'Zoom-api-Jwt-Request',
+      'content-type': 'application/json'
+    },
+    json: true //Parse the JSON string in the response
   };
 
   request(options, function (error, response, body) {
@@ -40,3 +33,30 @@ function userInformation(){
     console.log(body);
   });
 }
+
+function getZoomRoomDevices(){
+  var options = {
+    method: 'GET',
+    url: `https://api.zoom.us/v2/rooms/ImiKN5OuSn-41hV7D-ajag/devices`,
+    qs: {
+      status: 'active'
+    },
+    auth: {
+      'bearer': token
+    },
+    headers: {
+      'User-Agent': 'Zoom-api-Jwt-Request',
+      'content-type': 'application/json'
+    },
+    json: true //Parse the JSON string in the response
+  };
+
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+
+    console.log(body);
+  });
+}
+
+getZoomRoomDevices();
+getZoomRooms();
